d1505ee0a9e3b91733f15136a356183268151812
brandon111586 <brandon80229@gmail.com>
2022-02-12 01:55:15 +0800

    add setting_page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback, useMemo } from "react";
 import styled, { ThemeProvider } from "styled-components";
-import { ReactComponent as DayCloudyIcon } from "./images/day-cloudy.svg";
-import { ReactComponent as RainIcon } from "./images/rain.svg";
-import { ReactComponent as AirFlowIcon } from "./images/airFlow.svg";
-import { ReactComponent as RefreshIcon } from "./images/refresh.svg";
-import dayjs from "dayjs";
-import { ReactComponent as LoadingIcon } from "./images/loading.svg";
+import useWeatherAPI from "./hooks/useWeatherAPI";
+import { getMoment } from "./utils/helper";
+import WeatherCard from "./views/WeatherCard";
+import WeatherSetting from "./views/WeatherSetting";
 
 const Container = styled.div`
   background-color: ${({ theme }) => theme.backgroundColor};
@@ -13,103 +11,6 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-`;
-
-const WeatherCard = styled.div`
-  position: relative;
-  min-width: 360px;
-  box-shadow: ${({ theme }) => theme.boxShadow};
-  background-color: ${({ theme }) => theme.foregroundColor};
-  box-sizing: border-box;
-  padding: 30px 15px;
-`;
-
-const Location = styled.div`
-  font-size: 28px;
-  color: ${({ theme }) => theme.titleColor};
-  margin-bottom: 20px;
-`;
-
-const Description = styled.div`
-  font-size: 16px;
-  color: ${({ theme }) => theme.textColor};
-  margin-bottom: 30px;
-`;
-
-const CurrentWeather = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 30px;
-`;
-
-const Temperature = styled.div`
-  color: ${({ theme }) => theme.temperatureColor};
-  font-size: 96px;
-  font-weight: 300;
-  display: flex;
-`;
-
-const Celsius = styled.div`
-  font-weight: normal;
-  font-size: 42px;
-`;
-
-const AirFlow = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 16x;
-  font-weight: 300;
-  color: ${({ theme }) => theme.textColor};
-  margin-bottom: 20px;
-  svg {
-    width: 15px;
-    height: auto;
-    margin-right: 30px;
-  }
-`;
-
-const Rain = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 16x;
-  font-weight: 300;
-  color: ${({ theme }) => theme.textColor};
-  svg {
-    width: 25px;
-    height: auto;
-    margin-right: 30px;
-  }
-`;
-
-const Refresh = styled.div`
-  position: absolute;
-  right: 15px;
-  bottom: 15px;
-  font-size: 12px;
-  display: inline-flex;
-  align-items: flex-end;
-  color: ${({ theme }) => theme.textColor};
-  @keyframes rotate {
-    from {
-      transform: rotate(360deg);
-    }
-    to {
-      transform: rotate(0deg);
-    }
-  }
-  svg {
-    width: 15px;
-    height: 15px;
-    margin-left: 10px;
-    cursor: pointer;
-    animation: rotate infinite 1.5s linear;
-    animation-duration: ${({ isLoading }) => (isLoading ? "1.5s" : "0s")};
-  }
-`;
-
-const DayCloudy = styled(DayCloudyIcon)`
-  flex-basis: 30%;
 `;
 
 const theme = {
@@ -137,139 +38,31 @@ const LOCATION_NAME = "羅東";
 const LOCATION_NAME_FORECAST = "宜蘭縣";
 
 const App = () => {
-  console.log("invoke function component");
-  const [currentTheme, setCurrentTheme] = useState("light");
-  const [weatherElement, setWeatherElement] = useState({
-    locationName: " ",
-    description: " ",
-    windSpeed: 0,
-    temperature: 0,
-    rainPossibility: 0,
-    observationTime: new Date(),
-    comfortability: "",
-    weatherCode: 0,
-    isLoading: true,
+  const [weatherElement, fetchData] = useWeatherAPI({
+    locationName: LOCATION_NAME,
+    cityName: LOCATION_NAME_FORECAST,
+    authorizationKey: AUTHORIZATION_KEY,
   });
+  const [currentTheme, setCurrentTheme] = useState("dark");
+  const moment = useMemo(() => getMoment(LOCATION_NAME_FORECAST), []);
   useEffect(() => {
-    console.log("execute function in useEffect");
-    fetchCurrentWeather();
-    fetchWeatherForecast();
-  }, []);
-
-  const fetchCurrentWeather = () => {
-    console.log("execute function in useEffect");
-    setWeatherElement((preState) => ({ ...preState, isLoading: true }));
-    fetch(
-      `https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization=${AUTHORIZATION_KEY}&locationName=${LOCATION_NAME}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const locationData = data.records.location[0];
-
-        const weatherElements = locationData.weatherElement.reduce(
-          (neededElements, item) => {
-            if (["WDSD", "TEMP"].includes(item.elementName)) {
-              neededElements[item.elementName] = item.elementValue;
-            }
-            return neededElements;
-          },
-          {}
-        );
-
-        // STEP 3：要使用到 React 組件中的資料
-        setWeatherElement((prevState) => ({
-          ...prevState,
-          observationTime: locationData.time.obsTime,
-          locationName: locationData.locationName,
-          temperature: weatherElements.TEMP,
-          windSpeed: weatherElements.WDSD,
-          // description: "多雲時晴",
-          // rainPossibility: 60,
-          isLoading: false,
-        }));
-      });
-  };
-
-  const fetchWeatherForecast = () => {
-    fetch(
-      `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${AUTHORIZATION_KEY}&locationName=${LOCATION_NAME_FORECAST}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const locationData = data.records.location[0];
-        const weatherElements = locationData.weatherElement.reduce(
-          (neededElements, item) => {
-            if (["Wx", "PoP", "CI"].includes(item.elementName)) {
-              neededElements[item.elementName] = item.time[0].parameter;
-            }
-            return neededElements;
-          },
-          {}
-        );
-
-        setWeatherElement((prevState) => ({
-          ...prevState,
-          description: weatherElements.Wx.parameterName,
-          weatherCode: weatherElements.Wx.parameterValue,
-          rainPossibility: weatherElements.PoP.parameterName,
-          comfortability: weatherElements.CI.parameterName,
-        }));
-      });
-  };
-
-  const {
-    observationTime,
-    locationName,
-    description,
-    windSpeed,
-    temperature,
-    rainPossibility,
-    isLoading,
-    comfortability,
-  } = weatherElement;
+    setCurrentTheme(moment === "day" ? "light" : "dark");
+  }, [moment]);
+  const [currentPage, setCurrentPage] = useState("WeatherCard");
 
   return (
     <ThemeProvider theme={theme[currentTheme]}>
       <Container>
-        {console.log("render,isLoading:", isLoading)}
-        <WeatherCard>
-          <Location>{locationName}</Location>
-          <Description>
-            {description}
-
-            {comfortability}
-          </Description>
-          <CurrentWeather>
-            <Temperature>
-              {Math.round(temperature)}
-              <Celsius>°C</Celsius>
-            </Temperature>
-            <DayCloudy />
-          </CurrentWeather>
-          <AirFlow>
-            <AirFlowIcon />
-            {windSpeed} m/h
-          </AirFlow>
-          <Rain>
-            <RainIcon />
-            {rainPossibility}%
-          </Rain>
-          <Refresh
-            onClick={() => {
-              fetchCurrentWeather();
-              fetchWeatherForecast();
-            }}
-            isLoading={isLoading}
-          >
-            最後觀測時間：
-            {new Intl.DateTimeFormat("zh-TW", {
-              hour: "numeric",
-              minute: "numeric",
-            }).format(dayjs(observationTime))}
-            {""}
-            {isLoading ? <LoadingIcon /> : <RefreshIcon />}
-          </Refresh>
-        </WeatherCard>
+        {currentPage === "WeatherCard" && (
+          <WeatherCard
+            weatherElement={weatherElement}
+            moment={moment}
+            fetchData={fetchData}
+          ></WeatherCard>
+        )}
+        {currentPage === "WeatherSetting" && (
+          <WeatherSetting theme={theme[currentTheme]}></WeatherSetting>
+        )}
       </Container>
     </ThemeProvider>
   );
